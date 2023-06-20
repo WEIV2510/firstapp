fbdbaa9726105d8650942c55775d8e9a363c3291
WEIV2510 <ratthakarn.ma.65@ubu.ac.th>
2023-06-20 12:09:55 +0700

    HEADPUADMak

---
+++ src/Foooter.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function Footer(){
+    return
+    {
+        <div classname='MyFooter'>
+              <span>Copytight 2020 @RAtthakarn Mango</span>
+        </div>
+    };
+}
+export default Footer;
