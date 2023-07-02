7d8803f05e6e9c0c2d7fd7563796992395378e50
WEIV2510 <ratthakarn.ma.65@ubu.ac.th>
2023-07-02 19:27:38 +0700

    start

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
-import './App.css';
-import Header from './Header';
-import Footer from './Foooter';
 import { useState } from 'react';
+import './App.css';
+import MyFooter from './Footer';
+import MyHeader from './Header';
+
 function App() {
-  const [num,setNum] = useState(0);
+  setInterval(UpdateTime, 1000);
+  let now = new Date().toLocaleTimeString('th-TH');
+  const [time, setTime] = useState(now);
+  const [num, setNum] = useState(0);
+
+  function UpdateTime() {
+    let newTime = new Date().toLocaleTimeString('th-TH');
+    setTime(newTime);
+  }
+
   return (
-    <div className="App">
-      <Header />
-      <div className='MyContent'>
-        <h2>[num]</h2>
-        <button onClick={()=>setNum(num+1)}>เพิ่ม</button>
-        <button onClick={()=>setNum(num+1)}>ลด</button>
+  <div>
+    <Header />
+      <div className='My-content'>
+        <div>ขณะนี้เวลา</div>
+        <div>{time}</div>
+        <h2>{num}</h2>
+        <button onClick={()=>setNum(num+1)}>เพิ่ม</button><button onClick={()=>setNum(num-1)}>ลด</button>
       </div>
-      <Footer />
-    </div>
+    <Footer />
+  </div>
   );
 }
 
